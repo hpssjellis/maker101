b8e3c0e9bef0c05bf320522a83e1af8c8d66f7a3
Jeremy Ellis <keyfreemusic@gmail.com>
2022-07-17 11:08:31 -0700

    Update a03a-color-data-forwarder.ino

--- ml-kit-nano-33-ble-sense-examples/a03a-color-data-forwarder.ino
+++ ml-kit-nano-33-ble-sense-examples/a03a-color-data-forwarder.ino
@@ -1,5 +1,5 @@
-#include <Arduino_APDS9960.h>
-
+// for proximity, gesture, colour
+#include <Arduino_APDS9960.h> 
 
 #define FREQUENCY_HZ        50
 #define INTERVAL_MS         (1000 / (FREQUENCY_HZ + 1))
@@ -10,31 +10,28 @@ int proximity, gesture, colourR, colourG, colourB;
 void setup() {
     Serial.begin(115200);
     Serial.println("Started");
- APDS.setGestureSensitivity(70);  // 0 to 100
-  if (!APDS.begin())
-  {
-    Serial.println("Error initializing APDS9960 sensor.");
+    APDS.setGestureSensitivity(70);  // 0 to 100
+    if (!APDS.begin()){
+      Serial.println("Error initializing APDS9960 sensor.");
     /* Hacky way of stopping program executation in event of failure. */
-    while(1);
-  }
-  /* As per Arduino_APDS9960.h, 0=100%, 1=150%, 2=200%, 3=300%. Obviously more
-   * boost results in more power consumption. */
-  APDS.setLEDBoost(0);   
-
+      while(1);
+    }
+    // As per Arduino_APDS9960.h, 0=100%, 1=150%, 2=200%, 3=300%. Obviously more
+    // boost results in more power consumption. 
+    APDS.setLEDBoost(0);   
 }
 
 void loop() {
 
-
     if (millis() > last_interval_ms + INTERVAL_MS) {
-        last_interval_ms = millis();
-
-     //   IMU.readAcceleration(x, y, z);
-        APDS.readColor(colourR, colourG, colourB);
-        Serial.print(colourR * 1.0);
-        Serial.print('\t');
-        Serial.print(colourG * 1.0);
-        Serial.print('\t');
-        Serial.println(colourB * 1.0);
+       last_interval_ms = millis();
+       if ( APDS.colorAvailable()) {
+          APDS.readColor(colourR, colourG, colourB);
+          Serial.print(colourR * 1.0);
+          Serial.print('\t');
+          Serial.print(colourG * 1.0);
+          Serial.print('\t');
+          Serial.println(colourB * 1.0);
+       }
     }
 }
