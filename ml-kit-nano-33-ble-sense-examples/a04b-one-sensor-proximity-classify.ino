167a7ba012c1ad13ff423724b56a7e8091f5b702
Jeremy Ellis <keyfreemusic@gmail.com>
2022-07-20 23:06:02 -0700

    Create a04b-one-sensor-proximity-classify.ino

---
+++ ml-kit-nano-33-ble-sense-examples/a04b-one-sensor-proximity-classify.ino
@@ -0,0 +1,157 @@
+/* Edge Impulse Arduino examples
+ * Copyright (c) 2021 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+ 
+
+/* Includes ---------------------------------------------------------------- */
+
+//This is your RAW regression model made using EdgeImpulse.com
+#include <ei-proximity-v04_inferencing.h>
+
+
+
+
+/* For APDS9960 Gesture, light, and proximity sensor */
+#include <Arduino_APDS9960.h>
+
+//#include <Arduino_LSM9DS1.h>
+
+//#define CONVERT_G_TO_MS2    9.80665f
+#define FREQUENCY_HZ        EI_CLASSIFIER_FREQUENCY
+#define INTERVAL_MS         (1000 / (FREQUENCY_HZ + 1))
+
+
+int proximity, gesture, colourR, colourG, colourB;
+
+
+static unsigned long last_interval_ms = 0;
+// to classify 1 frame of data you need EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE values
+float features[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE];
+// keep track of where we are in the feature array
+size_t feature_ix = 0;
+
+void setup() {
+    Serial.begin(115200);
+    
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(4000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(4000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(4000);
+
+  //  if (!IMU.begin()) {
+  //      Serial.println("Failed to initialize IMU!");
+  //      while (1);
+  //  }
+    
+
+  /* Set sensitivity from 0 to 100. Higher is more sensitive. In
+   * my experience it requires quite a bit of experimentation to
+   * get this right, as if it is too sensitive gestures will always
+   * register as GESTURE_DOWN or GESTURE_UP and never GESTURE_LEFT or
+   * GESTURE_RIGHT. This can be called before APDS.begin() as it just
+   * sets an internal sensitivity value.*/
+  APDS.setGestureSensitivity(70);  // 0 to 100
+  if (!APDS.begin())
+  {
+    Serial.println("Error initializing APDS9960 sensor.");
+    /* Hacky way of stopping program executation in event of failure. */
+    while(1);
+  }
+  /* As per Arduino_APDS9960.h, 0=100%, 1=150%, 2=200%, 3=300%. Obviously more
+   * boost results in more power consumption. */
+  APDS.setLEDBoost(0);    
+    
+    
+}
+
+void loop() {
+
+    if (millis() > last_interval_ms + INTERVAL_MS) {
+     last_interval_ms = millis();
+     digitalWrite(LED_BUILTIN, LOW);
+
+     if (APDS.proximityAvailable()){
+       // read sensor data in exactly the same way as in the Data Forwarder example
+       // IMU.readAcceleration(x, y, z);
+       //  Serial.print(String(proximity) + ","+String(gesture) + ","+String(colourR) + ","+String(colourG) + ","+String(colourB) + ","); 
+        proximity = 240 +(APDS.readProximity()*-1);  
+  
+        // fill the features buffer
+        features[feature_ix++] = proximity * 1.0;  // to make it into a float
+       // features[feature_ix++] = colourG * 1.0;
+       // features[feature_ix++] = colourB * 1.0;
+
+        // features buffer full? then classify!
+        if (feature_ix == EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
+            delay(100);
+            digitalWrite(LED_BUILTIN, HIGH);
+            //Serial.println(feature_ix);
+            //Serial.write(fb.getBuffer(), cam.frameSize());
+            //ei_printf(feature_ix);
+            //Serial.write(feature_ix, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE );
+            //Serial.print(feature_ix[0]);
+            delay(100);
+            ei_impulse_result_t result;
+
+            // create signal from features frame
+            signal_t signal;
+            numpy::signal_from_buffer(features, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+
+            // run classifier
+            EI_IMPULSE_ERROR res = run_classifier(&signal, &result, false);
+           // ei_printf("run_classifier returned: %d\n", res);
+            //ei_printf("framesize: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+            if (res != 0) return;
+
+            // print predictions
+          //  ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.): \n",
+           //     result.timing.dsp, result.timing.classification, result.timing.anomaly);
+
+            // print the predictions
+            for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+                ei_printf("%s:\t%.5f\n", result.classification[ix].label, result.classification[ix].value);
+            }
+        #if EI_CLASSIFIER_HAS_ANOMALY == 1
+            ei_printf("anomaly:\t%.3f\n", result.anomaly);
+        #endif
+
+            // reset features frame
+            feature_ix = 0;
+        }
+    }
+  }  
+}
+
+void ei_printf(const char *format, ...) {
+    static char print_buf[1024] = { 0 };
+
+    va_list args;
+    va_start(args, format);
+    int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
+    va_end(args);
+
+    if (r > 0) {
+        Serial.write(print_buf);
+    }
+}
